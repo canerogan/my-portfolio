3f2107b0f2ffb8f5b431edd1501520f1b3334b3e
canerogan <canerizda@gmail.com>
2024-03-19 12:48:59 +0100

    TechStack verbessert

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-
 // Funktion für Darkmode
 function toggleDarkMode() {
-    // ändert CSS zu Darkmode 
-    document.querySelector('body').classList.toggle("dark-theme");
+  // ändert CSS zu Darkmode
+  document.querySelector("body").classList.toggle("dark-theme");
 
-    // ändert das Darkmode Icon aus dem HTML
-    var darkThemeIcon = document.getElementById('darktheme');
-    if (darkThemeIcon.src.endsWith('darktheme_icon.svg')) {
-      darkThemeIcon.src = './assets/darktheme_icon_reversed.svg'; // Change to the reversed icon
-    } 
-    else {
-      darkThemeIcon.src = './assets/darktheme_icon.svg'; // Change back to the original icon
-    }
-
+  // ändert das Darkmode Icon aus dem HTML
+  var darkThemeIcon = document.getElementById("darktheme");
+  if (darkThemeIcon.src.endsWith("darktheme_icon.svg")) {
+    darkThemeIcon.src = "./assets/darktheme_icon_reversed.svg"; // Change to the reversed icon
+  } else {
+    darkThemeIcon.src = "./assets/darktheme_icon.svg"; // Change back to the original icon
+  }
 }
+
+// EasterEgg Aktion
+
+document.addEventListener("DOMContentLoaded", easterEggAction);
+
+function easterEggAction() {
+  let eastereggButton = document.getElementById("eastereggButton");
+  let eastereggDiv = document.getElementById("easteregg");
+
+  if (eastereggButton && eastereggDiv) {
+      eastereggButton.addEventListener("click", function() {
+        // Ternary-Abfrage:
+        // Bedingung ? AusdruckWennWahr : AusdruckWennFalsch
+          eastereggDiv.style.display = eastereggDiv.style.display !== "none" ? "none" : "block";
+      });
+  }
+}
+
